c5ca0b1482bba55c117732646aa70263ee6bc152
Chong Han Chua <johncch@gmail.com>
2020-05-18 10:07:18 -0700

    Add new data and some date time formatting

--- src/components/item.js
+++ src/components/item.js
@@ -1,52 +1,75 @@
 import React from "react"
 import { css } from "@emotion/core"
 
-const Item = ({ name, birth, death, location, details, source }) => (
-  <div
-    id={`${name
-      .toLowerCase()
-      .split(" ")
-      .join("-")}`}
-    css={css`
-      border-bottom: 1px solid rgba(255, 255, 255, 0.4);
-      text-align: center;
-      box-sizing: border-box;
+const yFormatter = new Intl.DateTimeFormat("en", { year: "numeric" })
+const mFormatter = new Intl.DateTimeFormat("en", { month: "short" })
+const dFormatter = new Intl.DateTimeFormat("en", { day: "2-digit" })
 
-      &:last-child {
-        border-bottom: 0;
-      }
+const formatDate = dateStr => {
+  if (dateStr) {
+    const yearOnly = dateStr.length <= 4
+    const date = new Date(dateStr)
+    const year = yFormatter.format(date)
 
-      &:target:before {
-        content: " ";
-        display: block;
-        height: 150px;
-        margin-top: -150px;
-        visibility: hidden;
-      }
-    `}
-  >
+    if (yearOnly) {
+      return year
+    }
+    const month = mFormatter.format(date)
+    const d = dFormatter.format(date)
+    const output = `${month} ${d}, ${year}`
+    return output
+  }
+  return "?"
+}
+
+const Item = ({ name, birth, death, location, details, source }) => {
+  return (
     <div
+      id={`${name
+        .toLowerCase()
+        .split(" ")
+        .join("-")}`}
       css={css`
-        padding: 1rem;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.4);
+        text-align: center;
+        box-sizing: border-box;
+
+        &:last-child {
+          border-bottom: 0;
+        }
+
+        &:target:before {
+          content: " ";
+          display: block;
+          height: 150px;
+          margin-top: -150px;
+          visibility: hidden;
+        }
       `}
     >
-      <h3>{name}</h3>
-      <p>
-        {birth || "?"}–{death}
-      </p>
-      <p>{details}</p>
-      <p>{location}</p>
-      {source ? (
-        <p
-          css={css`
-            font-size: 0.8em;
-          `}
-        >
-          <a href={source}>[source]</a>
+      <div
+        css={css`
+          padding: 1rem;
+        `}
+      >
+        <h3>{name}</h3>
+        <p>
+          {formatDate(birth)}–{formatDate(death)}
         </p>
-      ) : null}
+        <p>{details}</p>
+        <p>{location}</p>
+        {source ? (
+          <p
+            css={css`
+              font-size: 0.8em;
+            `}
+          >
+            <a href={source}>[source]</a>
+          </p>
+        ) : null}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Item
